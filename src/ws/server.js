125c474ab4a439200a85892d5ba7de43a8494d86
Sanu Islam <sanuvipbd@gmail.com>
2026-03-23 14:48:37 +0600

    update server and some code

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,9 +1,7 @@
 import { WebSocket, WebSocketServer } from "ws";
 
 function sendJson(socket, payload) {
-  if (socket.readyState !== WebSocket.OPEN) {
-    return;
-  }
+  if (socket.readyState !== WebSocket.OPEN) return;
 
   socket.send(JSON.stringify(payload));
 }
@@ -23,10 +21,27 @@ export function attachWebSocketServer(server) {
   });
 
   wss.on("connection", (socket) => {
+    socket.isAlive = true;
+    socket.on("pong", () => {
+      socket.isAlive = true;
+    });
+
     sendJson(socket, { type: "welcome" });
 
     socket.on("error", console.error);
   });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) {
+        return ws.terminate();
+      }
+      ws.isAlive = false;
+      ws.ping();
+    });
+  }, 30000);
+
+  wss.on("close", () => clearInterval(interval));
 
   function broadcastMatchCreated(match) {
     broadcast(wss, { type: "match_created", data: match });
